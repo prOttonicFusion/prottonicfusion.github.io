80dca3c432f9056efc709577f742767e27235fe9
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-28 02:55:51 +0200

    create project card component

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,44 +1,28 @@
 import React from "react"
-import {Card, Text, Link, Image, Box, Heading} from "theme-ui"
+import { Image } from "theme-ui"
+import SocialIcon from "./social-icon"
+import "../styles/global.scss"
 
-const ProjectCard = ({title, link, bg, textColor, thumbnailPath, children}) => (
-    <Box
-        sx={{
-            "&:hover": {
-                transform: `translateY(-5px)`,
-                boxShadow: `xl`,
-            },
-        }}
-    >
-        <Link href={link}
-           target="_blank"
-           rel="noreferrer noopener"
-           sx={{
-               textDecoration: "none !important",
-               color: textColor,
-           }}
-        >
-            <Card
-                p={2}
-                sx={{
-                    background: bg,
-                }}
-            >
-
-                {thumbnailPath &&
-
-                    <Box sx={{
-                        textAlign: 'center'
-                    }}>
-                        <Image
-                            src={thumbnailPath}/>
-                    </Box>
-                }
-                <Heading pb={3}>{title}</Heading>
-                <Text>{children}</Text>
-            </Card>
-        </Link>
-    </Box>
+const ProjectCard = ({ title, image, description, wordpress, github, link, documentation }) => (
+    <div class="card" style={{ width: "18rem" }}>
+        <Image class="card-img-top" src={image} alt="Card image cap"></Image>
+        <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{description}</p>
+            {github &&
+                <SocialIcon service="github" url={github} label="GitHub"></SocialIcon>
+            }
+            {wordpress &&
+                <SocialIcon service="wordpress" url={wordpress} label="WordPress"></SocialIcon>
+            }
+            {documentation &&
+                <SocialIcon service="documentation" url={documentation} label="Docs"></SocialIcon>
+            }
+            {link &&
+                <SocialIcon service="" url={link} label="WebSite"></SocialIcon>
+            }
+        </div>
+    </div>
 )
 
 export default ProjectCard;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,36 @@
 import React from "react"
+import yaml from 'js-yaml'
 import ProjectsMDX from "../sections/projects"
 import { Grid } from "theme-ui"
+import ProjectCard from "./project-card"
+import projectList from "../data/projects.json"
 
 const Projects = () => (
     <Grid
         pt={100}
         pb={100}
-        columns={[ 1, 2 ]}
+        columns={[1, 2, 3]}
         id="projects"
         sx={{
             h1: { gridColumn: `-1/1` },
+            justifyItems: 'center',
         }}
     >
-        <ProjectsMDX/>
+        <ProjectsMDX />
+
+        {projectList[0].projects.map((project) => {
+            return <ProjectCard
+                title={project.name}
+                description={project.description}
+                image={project.image}
+                github={project.github}
+                wordpress={project.wordpress}
+                documentation={project.documentation}
+                link={project.link}
+            >
+            </ProjectCard>
+        })}
+
     </Grid>
 )
 
