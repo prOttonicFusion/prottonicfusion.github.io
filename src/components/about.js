6a60ddc9f23816f0a334a2edb3c45d58e9a749e4
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-30 16:15:06 +0200

    tweak page layout

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import { Grid, Box, Image } from "theme-ui"
 import "../styles/global.scss"
 
 const About = () => (
-  <Grid pt={50} pb={100} pl={4} pr={4} columns={[1, 1, 1, 1, 2]} id="about">
+  <Grid pt={30} pb={100} pl={4} pr={4} columns={[1, 1, 1, 1, 2]} id="about">
     <Box>
       <AboutMDX />
     </Box>
