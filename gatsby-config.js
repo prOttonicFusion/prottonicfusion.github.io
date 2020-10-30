30e9369bfb2d1dd3e042aa1b2f48fde34fe9d6d0
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-30 13:52:42 +0200

    refactor contact section

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,23 @@ module.exports = {
     siteUrl: `https://www.gatsbyjs.com`,
     description: `Just another sample site`,
     author: `prOttonicFusion`,
+    contactOptions: [
+      {
+        name: "github",
+        label: "GitHub",
+        url: "https://github.com/prottonicfusion/",
+      },
+      {
+        name: "linkedin",
+        label: "LinkedIn",
+        url: "https://www.linkedin.com/in/otto-lindblom",
+      },
+      {
+        name: "email",
+        label: "E-mail",
+        url: "mailto: otto.lindblom@gmail.com",
+      }
+    ],
     menuLinks: [
       {
         name: "About",
