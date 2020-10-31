3126ec8891998b4a3d0e2a15965491bd646f2ce8
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-31 02:21:07 +0200

    create skills component

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,49 +1,76 @@
 import React from "react"
 import SkillsMDX from "../sections/skills"
-import { Grid, Box, Image } from "theme-ui"
-import { Carousel } from "react-bootstrap"
+import { Grid, Box } from "theme-ui"
+import { Tab, Row, Col, Nav } from "react-bootstrap"
+import skillsData from "../data/skills.json"
 import "../styles/global.scss"
+
+const SkillItem = (props) => {
+  const { name, badges, level } = props
+  return (
+    <Row className="skill-item">
+      <Col className="skill-description-container">
+        <span className="skill-title">{name}</span>
+        <span className="skill-badge-container">
+          {badges.map((badge) => (
+            <img src={badge} className="skill-badge" height={"15px"} key={badge} />
+          ))}
+        </span>
+      </Col>
+      <Col className="skill-level-container">
+        <span className="progress-bar">
+          <span className="progress" style={{ width: `${level}%` }}></span>
+        </span>
+      </Col>
+    </Row>
+  )
+}
+
+const SkillSheet = (props) => {
+  return (
+    <div className={"skill-sheet"}>
+      {props.skills.map((skill) => (
+        <SkillItem name={skill.title} level={skill.level} badges={skill.badges} key={skill.title} />
+      ))}
+    </div>
+  )
+}
 
 const Skills = () => (
   <Grid pt={30} pb={100} pl={4} pr={4} columns={[1, 1, 1, 1, 2]} id="skils">
     <Box>
       <SkillsMDX />
     </Box>
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="0">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            {skillsData.map((category, index) => (
+              <Nav.Item key={category.categoryTitle}>
+                <Nav.Link eventKey={index}>{category.categoryTitle}</Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            {skillsData.map((category, index) => (
+              <Tab.Pane eventKey={index} key={category.categoryTitle}>
+                <SkillSheet skills={category.skills} />
+              </Tab.Pane>
+            ))}
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+    {/**Carousel>
+      {skillsData.map((category) => (
+        <Carousel.Item interval={5000} key={category}>
+          <h3>{category.categoryTitle}</h3>
+          <SkillSheet skills={category.skills} />
+        </Carousel.Item>
+      ))}
+      </Carousel> */}
   </Grid>
 )
 
