3466cea9542e56da5f4265c29b52b702e55620a0
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-11-01 17:41:00 +0200

    refactor banner

--- src/templates/default-template.js
+++ src/templates/default-template.js
@@ -6,12 +6,26 @@ import Projects from "../components/projects"
 import Skills from "../components/skills"
 import Contact from "../components/contact"
 import Footer from "../components/footer"
-import { Divider } from "theme-ui"
+import { Divider, Image, Box } from "theme-ui"
+import profilePic from "../assets/images/profile.jpg"
+import BannerMDX from "../sections/banner"
 import SEO from "../components/seo"
 
 const DefaultTemplate = () => (
   <>
-    <Banner />
+    <Banner>
+      <Image
+        src={profilePic}
+        sx={{
+          maxWidth: "20%",
+          minWidth: "200px",
+        }}
+        className={"profile-pic"}
+      />
+      <Box>
+        <BannerMDX />
+      </Box>
+    </Banner>
     <Layout>
       <SEO title="Home" />
       <About />
