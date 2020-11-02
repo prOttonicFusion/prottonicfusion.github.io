21b6ce11941481949a7e2b0ce90416dee894a425
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-11-02 14:29:23 +0200

    refactor timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,31 +4,42 @@ import { Grid, Box } from "theme-ui"
 import { Tab, Row, Col, Nav } from "react-bootstrap"
 import timelineData from "../data/timeline.json"
 import "../styles/global.scss"
+import "../styles/timeline.scss"
 
-const TimelineItem = ({eventData}) => {
-    return (
-        <div class={eventData.direction}>
-            <div class="timeline-flag-wrapper">
-                <span class="timeline-flag">{eventData.title}</span>
-                <span class="timeline-time-wrapper"><span class="timeline-time">{eventData.time}</span></span>
-            </div>
-            <div class="timeline-place">{eventData.place}</div>
+const TimelineItem = ({ eventData }) => {
+  return (
+    <li className={eventData.itemClass}>
+      <div class="timeline-badge">
+        <i class="glyphicon glyphicon-check"></i>
+      </div>
+      <div class="timeline-panel">
+        <div class="timeline-heading">
+          <h4 class="timeline-title">{eventData.title}</h4>
+          <p class="timeline-place-name">@ {eventData.place}</p>
+          <p>
+            <small class="timeline-time">
+              {eventData.time}
+            </small>
+          </p>
         </div>
-    )
+        <div class="timeline-body">
+          <p>{eventData.description}</p>
+        </div>
+      </div>
+    </li>
+  )
 }
 
 const Timeline = () => (
-  <Grid pt={100} pb={100} pl={4} pr={4} columns={[1, 1, 1, 1, 2]} id="skills">
+  <Grid pt={30} pb={30} pl={4} pr={4} columns={[1, 1, 1, 1, 2]} id="skills">
     <Box>
       <TimelineMDX />
     </Box>
     <div className="timeline-container">
       <ul class="timeline">
-        <li>
-          {timelineData.map((data) => (
-            <TimelineItem eventData={data} />
-          ))}
-        </li>
+        {timelineData.map((data) => (
+          <TimelineItem eventData={data} />
+        ))}
       </ul>
     </div>
   </Grid>
