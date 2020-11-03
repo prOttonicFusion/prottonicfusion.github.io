a710780fbd976ad4f139a344776fd3a26773f9ef
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-11-04 00:29:04 +0200

    add page outline

---
+++ src/pages/brief-cv.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import DefaultTemplate from "../templates/default-template"
+
+export default function BriefCVPage() {
+  return <DefaultTemplate location="/brief-cv" />
+}
