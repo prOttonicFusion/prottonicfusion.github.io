5f197641fba4600be1deed08cfe61a4822db21e8
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-11-10 00:01:13 +0200

    tweak card style

--- src/components/blog/blog-post-card.js
+++ src/components/blog/blog-post-card.js
@@ -6,10 +6,12 @@ import "../../styles/blog.scss"
 const BlogPostCard = ({ title, image, excerpt, date, author, url }) => (
   <div className="post-card">
     <div className="post-card-description">
-      <h1 className="post-card-title">
-        <a href={url}>{title}</a>
-      </h1>
-      <p className="post-card-excerpt">{excerpt}</p>
+      <div>
+        <h1 className="post-card-title">
+          <a href={url}>{title}</a>
+        </h1>
+        <p className="post-card-excerpt">{excerpt}</p>
+      </div>
       <p className="post-card-info">
         {author}, <span className="post-date">{date}</span>
       </p>
