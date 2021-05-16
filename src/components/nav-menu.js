8e48c9d45ef872756ca7c74c0c27fe2a7a82c787
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2021-05-17 00:26:23 +0300

    show x icon when nav menu is expanded

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-const NavMenu = ({ location }) => (
-  <StaticQuery
-    query={graphql`
+const NavMenu = ({ location }) => {
+
+  const [state, setState] = useState({ menuIsOpen: false })
+
+  const toggleMenuState = () => setState({ ...state, menuIsOpen: !state.menuIsOpen })
+
+  return (
+    <StaticQuery
+      query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -16,49 +22,48 @@ const NavMenu = ({ location }) => (
         }
       }
     `}
-    render={(data) => (
-      <div className="site-nav-container">
-        {location !== "/" && (
-          <a className="site-title" href={"/"}>
-            {data.site.siteMetadata.title}
-          </a>
-        )}
-        <nav className="site-nav">
-          <input type="checkbox" id="nav-trigger" className="nav-trigger" />
-          <label htmlFor="nav-trigger">
-            <span className="nav-menu-icon">
-              <svg viewBox="0 0 18 15" width="18px" height="15px">
-                <path
-                  fill="#424242"
-                  d="M18,1.484c0,0.82-0.665,1.484-1.484,1.484H1.484C0.665,2.969,0,2.304,0,1.484l0,0C0,0.665,0.665,0,1.484,0 h15.031C17.335,0,18,0.665,18,1.484L18,1.484z"
-                />
-                <path
-                  fill="#424242"
-                  d="M18,7.516C18,8.335,17.335,9,16.516,9H1.484C0.665,9,0,8.335,0,7.516l0,0c0-0.82,0.665-1.484,1.484-1.484 h15.031C17.335,6.031,18,6.696,18,7.516L18,7.516z"
-                />
-                <path
-                  fill="#424242"
-                  d="M18,13.516C18,14.335,17.335,15,16.516,15H1.484C0.665,15,0,14.335,0,13.516l0,0 c0-0.82,0.665-1.484,1.484-1.484h15.031C17.335,12.031,18,12.696,18,13.516L18,13.516z"
-                />
-              </svg>
-            </span>
-          </label>
+      render={(data) => (
+        <div className="site-nav-container">
+          {location !== "/" && (
+            <a className="site-title" href={"/"}>
+              {data.site.siteMetadata.title}
+            </a>
+          )}
+          <nav className="site-nav">
+            <input type="checkbox" id="nav-trigger" className="nav-trigger" onClick={toggleMenuState} />
+            <label htmlFor="nav-trigger">
+              <span className="nav-menu-icon">
+                {!state.menuIsOpen &&
+                  // https://heroicons.com/
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 20 20" fill="#424242">
+                    <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+                  </svg>
+                }
+                {state.menuIsOpen &&
+                  // https://heroicons.com/
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 20 20" fill="#424242">
+                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                  </svg>
+                }
+              </span>
+            </label>
 
-          <div className="trigger">
-            {data.site.siteMetadata.menuLinks.map((menuLink) => (
-              <a
-                className="custom-nav-link"
-                href={menuLink.link}
-                key={menuLink.name}
-              >
-                {menuLink.name}
-              </a>
-            ))}
-          </div>
-        </nav>
-      </div>
-    )}
-  />
-)
+            <div className="trigger">
+              {data.site.siteMetadata.menuLinks.map((menuLink) => (
+                <a
+                  className="custom-nav-link"
+                  href={menuLink.link}
+                  key={menuLink.name}
+                >
+                  {menuLink.name}
+                </a>
+              ))}
+            </div>
+          </nav>
+        </div>
+      )}
+    />
+  )
+}
 
 export default NavMenu
