f3c600210db6011fccd64550a57c963436ab413b
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2022-05-18 13:33:46 +0300

    Chore/bump gatsby (#279)
    * bump gatsby plugins
    
    * bump gatsby to v4.14.0
    
    * update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
+// Configured in accordance with
+// https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/#2-creating-a-configuration-file-for-jest
+
 module.exports = {
     testEnvironment: 'jsdom',
     testRunner: 'jest-jasmine2',
@@ -7,6 +10,12 @@ module.exports = {
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': 'babel-jest',
         '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
+        '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/dist/$1', // Workaround for https://github.com/facebook/jest/issues/9771
+        '^gatsby-core-utils/(.*)$': 'gatsby-core-utils/dist/$1', // Workaround for https://github.com/facebook/jest/issues/9771
+        '^gatsby-plugin-utils/(.*)$': [
+            'gatsby-plugin-utils/dist/$1',
+            'gatsby-plugin-utils/$1',
+        ], // Workaround for https://github.com/facebook/jest/issues/9771
     },
     testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
     transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
