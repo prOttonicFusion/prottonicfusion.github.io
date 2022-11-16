b017d285da7cfd0532833b5eaf9ea6d81cbc1a68
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2022-11-16 14:56:51 +0200

    use offcanvas nav

--- src/components/icons/hamburger-menu.js
+++ src/components/icons/hamburger-menu.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const HamburgerMenuIcon = ({ width = '24px', height = '24px', fill = '#424242' }) =>
+const HamburgerMenuIcon = ({ width = '24px', height = '24px', fill = '#424242', ...rest }) =>
     // https://heroicons.com/
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...{ width, height, fill }}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...{ width, height, fill, ...rest }}>
         <path
             fillRule="evenodd"
             d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"
