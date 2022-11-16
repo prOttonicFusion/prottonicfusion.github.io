9f5daf6c30ec7403820ee94d405d87997d646cf2
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2022-11-16 15:59:41 +0200

    properly mock window.matchMedia

--- src/components/__tests__/banner.test.js
+++ src/components/__tests__/banner.test.js
@@ -3,6 +3,21 @@ import { render } from '@testing-library/react'
 import { withQuerySnapshot } from 'gatsby-plugin-testing'
 import Banner from '../banner'
 
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+})
+
 describe('Page banner', () => {
     it('renders root page banner correctly', withQuerySnapshot(() => {
         const { container } = render(
