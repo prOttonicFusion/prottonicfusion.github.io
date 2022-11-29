ec928cc2f6a5833eb211fd1630c8407aa1334a8a
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2022-11-29 12:25:05 +0200

    drop usage of StaticQuery

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,42 +1,42 @@
 import '../styles/global.scss'
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import SocialIcon from './social-icon'
 
-const Footer = () =>
-    <StaticQuery
-        query={graphql`
-          query ContactOptionsQuery {
-            site {
-              siteMetadata {
-                contactOptions {
-                  name
-                  label
-                  url
-                }
-                author
-              }
+const Footer = () => {
+    const data = useStaticQuery(graphql`
+      query ContactOptionsQuery {
+        site {
+          siteMetadata {
+            contactOptions {
+              name
+              label
+              url
             }
+            author
           }
-        `}
-        render={(data) =>
-            <div className={'site-footer'}>
-                <div className={'social-icon-container'}>
-                    {data.site.siteMetadata.contactOptions.map((contactOption) =>
-                        <SocialIcon
-                            service={contactOption.name}
-                            url={contactOption.url}
-                            label={contactOption.label}
-                            key={contactOption.url}
-                        ></SocialIcon>,
-                    )}
-                </div>
-                <div className="copyright-container">
-                    {new Date().getFullYear()} &copy; {data.site.siteMetadata.author}
-                </div>
-            </div>
         }
-    />
+      }
+    `)
+
+    return (
+        <div className={'site-footer'}>
+            <div className={'social-icon-container'}>
+                {data.site.siteMetadata.contactOptions.map((contactOption) =>
+                    <SocialIcon
+                        service={contactOption.name}
+                        url={contactOption.url}
+                        label={contactOption.label}
+                        key={contactOption.url}
+                    ></SocialIcon>,
+                )}
+            </div>
+            <div className="copyright-container">
+                {new Date().getFullYear()} &copy; {data.site.siteMetadata.author}
+            </div>
+        </div>
+    )
+}
 
 
 export default Footer
--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -1,63 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Offcanvas from 'react-bootstrap/Offcanvas'
 import HamburgerMenuIcon from './icons/hamburger-menu'
 
-const NavMenu = ({ location }) =>
-    <StaticQuery
-        query={graphql`
-                query SiteTitleQuery {
-                    site {
-                        siteMetadata {
-                            title
-                            menuLinks {
-                                name
-                                link
-                            }
-                        }
+const NavMenu = ({ location }) => {
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                    menuLinks {
+                        name
+                        link
                     }
                 }
-            `}
-        render={(data) =>
-            <Navbar expand="md" variant="dark" bg="none" className="site-nav" collapseOnSelect>
-                <Container fluid>
-                    {location !== '/' &&
-                            <Navbar.Brand className="site-title" href={'/'}>{data.site.siteMetadata.title}</Navbar.Brand>
-                    }
-                    <Navbar.Toggle aria-controls={'offcanvasNavbar-expand'} className="nav-toggle">
-                        <HamburgerMenuIcon className="nav-toggle-icon" width="32px" height="32px" />
-                    </Navbar.Toggle>
-                    <Navbar.Offcanvas
-                        id={'offcanvasNavbar-expand'}
-                        aria-labelledby={'offcanvasNavbarLabel-expand'}
-                        placement="top"
-                        restoreFocus={false}
-                        style={{ height: 'fit-content' }}
-                    >
-                        <Offcanvas.Header closeButton />
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                {data.site.siteMetadata.menuLinks.map(
-                                    (menuLink) =>
-                                        <Nav.Link
-                                            className="custom-nav-link"
-                                            href={menuLink.link}
-                                            key={menuLink.name}
-                                        >
-                                            {menuLink.name}
-                                        </Nav.Link>,
-                                )}
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
+            }
         }
-    />
+    `)
+
+    return (
+        <Navbar expand="md" variant="dark" bg="none" className="site-nav" collapseOnSelect>
+            <Container fluid>
+                {location !== '/' &&
+                        <Navbar.Brand className="site-title" href={'/'}>{data.site.siteMetadata.title}</Navbar.Brand>
+                }
+                <Navbar.Toggle aria-controls={'offcanvasNavbar-expand'} className="nav-toggle">
+                    <HamburgerMenuIcon className="nav-toggle-icon" width="32px" height="32px" />
+                </Navbar.Toggle>
+                <Navbar.Offcanvas
+                    id={'offcanvasNavbar-expand'}
+                    aria-labelledby={'offcanvasNavbarLabel-expand'}
+                    placement="top"
+                    restoreFocus={false}
+                    style={{ height: 'fit-content' }}
+                >
+                    <Offcanvas.Header closeButton />
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            {data.site.siteMetadata.menuLinks.map(
+                                (menuLink) =>
+                                    <Nav.Link
+                                        className="custom-nav-link"
+                                        href={menuLink.link}
+                                        key={menuLink.name}
+                                    >
+                                        {menuLink.name}
+                                    </Nav.Link>,
+                            )}
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    )
+}
 
 
 NavMenu.propTypes = {
