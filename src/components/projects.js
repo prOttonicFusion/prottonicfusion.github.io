7a6c7d40515abdf093d83bba03cd91a30efc0d71
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2022-11-29 11:09:03 +0200

    show project carousel on mobile

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ProjectsMDX from '../sections/projects'
 import ProjectCard from './project-card'
 import projectList from '../data/projects.json'
+import { Carousel } from 'react-bootstrap'
 
 const Projects = () =>
     <div
@@ -16,8 +17,29 @@ const Projects = () =>
         <ProjectsMDX />
 
         <div className="project-cards-container">
-            {projectList[0].projects.map((project) => {
-                return (
+            {projectList[0].projects.map((project) =>
+                <ProjectCard
+                    title={project.name}
+                    description={project.description}
+                    image={project.image}
+                    github={project.github}
+                    wordpress={project.wordpress}
+                    documentation={project.documentation}
+                    link={project.link}
+                    key={project.name}
+                    keywords={project.keywords}
+                />
+            )}
+        </div>
+        <Carousel
+            variant="dark"
+            interval={null}
+            controls={true}
+            indicators={false}
+            className="project-cards-container-mobile"
+        >
+            {projectList[0].projects.map((project) =>
+                <Carousel.Item key={project.name}>
                     <ProjectCard
                         title={project.name}
                         description={project.description}
@@ -26,12 +48,11 @@ const Projects = () =>
                         wordpress={project.wordpress}
                         documentation={project.documentation}
                         link={project.link}
-                        key={project.name}
                         keywords={project.keywords}
                     />
-                )
-            })}
-        </div>
+                </Carousel.Item>
+            )}
+        </Carousel>
     </div>
 
 
